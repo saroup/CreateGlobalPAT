526c36121f8bb77509e95128468eab43d1d6ecd1
Samantha Rouphael <saroup@microsoft.com>
2022-01-23 21:56:18 -0500

    Adding files for the initial commit for the tool

---
+++ CreateGlobalPAT/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.Services.Client;
+using Microsoft.VisualStudio.Services.WebApi;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateGlobalPAT
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            var connection = new VssConnection(new Uri("https://spsprodwcus0.vssps.visualstudio.com/A850a26fd-8300-ce32-bb6e-28e032a3a0fd"), new VssClientCredentials(false));
+            await connection.ConnectAsync();
+
+            var tokenClient = connection.GetClient<Microsoft.VisualStudio.Services.DelegatedAuthorization.WebApi.TokenHttpClient>();
+            var token = new Microsoft.VisualStudio.Services.DelegatedAuthorization.SessionToken()
+            {
+                TargetAccounts = new List<Guid>() { Guid.Parse("2663b13f-50e3-a655-a159-22f6f4725fab") },
+                Scope = "vso.gallery_publish",
+                DisplayName = "Global Marketplace publish PAT",
+                ValidTo = DateTime.Now.AddMonths(1)
+            };
+
+            var result = await tokenClient.CreateSessionTokenAsync(token, Microsoft.VisualStudio.Services.DelegatedAuthorization.SessionTokenType.Compact, false);
+            string json = JsonConvert.SerializeObject(result, Formatting.Indented);
+            Console.WriteLine(json);
+            Console.ReadKey();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # CreateGlobalPAT
-Tool to help create a global PAT
+
+## Deploy your dev extension to Azure DevOps
+
+Per current documentation to deploy your extension to the marketplace you should use the following command:
+
+```shell
+tfx extension publish --manifest-globs vss-extension.json --token [token]
+```
+
+The [token] here is the Azure DevOps personal access token with the Marketplace (Publish) scope and access set to **All accessible organizations**.
+
+Note that the instructions  say to use a global PAT, but the UI doesn't allow one to select this GUID.
+
+This small tool will help you create a global PAT that will help you publish your extension.
+Be careful when using personal access tokens with broad access across the organizations you have access to.
